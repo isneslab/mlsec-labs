808442b3b87ff3f3e51c10d7e522fe43eb42d33b
Fabio Pierazzi <pierazzi.fabio@gmail.com>
2024-04-12 12:49:00 +0200

    New solution for lab 04 - fixed

--- solutions/lab04-solution.py
+++ solutions/lab04-solution.py
@@ -1,3 +1,9 @@
+import sklearn.metrics as sm
+
+# It is important to have y as a numpy array before doing this operation for the mask
+mask_gw = (np.array(y)==0)
+mask_mw = (np.array(y)==1)
+
 ## Dataset 1
 
 mask_D1 = (mask_google & mask_gw) | (mask_else & mask_mw)
@@ -5,12 +11,12 @@ mask_D1 = (mask_google & mask_gw) | (mask_else & mask_mw)
 X_train, X_test, y_train, y_test = train_test_split(
     X[mask_D1], y[mask_D1], test_size=0.33, random_state=42)
 
-clf = LinearSVC(C=1)
+clf = LinearSVC(C=1, dual=True, max_iter=10000)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
-import sklearn.metrics as sm
-sm.f1_score(y_true=y_test, y_pred=y_pred)
+
+print('D1 - Google Play vs Other Markets - F1: {:.2f}'.format(sm.f1_score(y_true=y_test, y_pred=y_pred)))
 
 
 ## Dataset 2
@@ -20,9 +26,9 @@ mask_D2 = mask_google
 X_train, X_test, y_train, y_test = train_test_split(
     X[mask_D2], y[mask_D2], test_size=0.33, random_state=42)
 
-clf = LinearSVC(C=1)
+clf = LinearSVC(C=1, dual=True, max_iter=10000)
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
 import sklearn.metrics as sm
-sm.f1_score(y_true=y_test, y_pred=y_pred)
+print('D2 - Google Play only - F1: {:.2f}'.format(sm.f1_score(y_true=y_test, y_pred=y_pred)))
